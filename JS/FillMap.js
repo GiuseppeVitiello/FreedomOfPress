dfb67d124f21e562fc997ce4d68375733801fc68
DrogHead <droggay0636@gmail.com>
2021-05-28 19:53:12 +0200

    sonoFuoriDalBody

--- JS/FillMap.js
+++ JS/FillMap.js
@@ -1,5 +1,3 @@
-
-
 let countries = [
     {
         alt: "Norway",
@@ -8,7 +6,7 @@ let countries = [
         linksTo: "#Norway",
         desc: "Freedom of press in Norway dates back to the Constitution of 1814, and has since then promoted freedom of Expression and freedom of press. Most of the Norwegian press is private and self-regulates, allowing the freedom of press to be preserved. The government still provides support and actively promotes high-quality journalism and attempts to counter fake news and hate speech.<br><br>The new media responsibility laws, however, are considered too strict by media representatives. Particularly during the pandemic, the government was criticised for refusing to publish data on crisis management and for covering the pandemic’s impact on hospitals.",
         rank: "1st",
-        color: "rgb(220, 220, 220)",
+        color: "white",
     },
     {
         alt: "Sweden",
@@ -17,7 +15,7 @@ let countries = [
         linksTo: "#Sweden",
         desc: "The 1766 Freedom of the Press Act of the Swedish Constitution is regarded as the world's first law to support freedom of press and freedom of information. While this country always ranks in the top five of the RSF World Press Freedom Index, this doesn’t mean that it does not present any flaws: media ownership is highly condensed, with over 90% of the daily press controlled by only six owners.<br><br>During the pandemic, the government negatively impacted its principle of openness, since authorities refused to share state-held documents regarding the Covid-19 situation to the Press. In recent years a fall in advertising earnings put many media sources in difficult financial situations, resulting in the number of state subsidies to more than double.",
         rank: "3rd",
-        color: "rgb(220, 220, 220)",
+        color: "white",
     },
     {
         alt: "Costa Rica",
@@ -26,7 +24,7 @@ let countries = [
         linksTo: "#CostaRica",
         desc: "Differently from the neighbouring corruption-ridden Latin American countries, in which violent crimes and violence against the media are far too common, Costa Rica has a remarkable record on respecting freedom of expression, of press and human rights.<br><br>Authorities' interference in the media is quite rare, just as cases of attacks or threats against journalists, who are free to work and also supported by progressive legislation. The overly complicated procedures to access state-held information is one of the few negatives, together with media ownership being concentrated in very few hands.",
         rank: "5th",
-        color: "gainsboro",
+        color: "white",
     },
     {
         alt: "Italy",
@@ -35,7 +33,7 @@ let countries = [
         linksTo: "#Italy",
         desc: "In Italy, freedom of press was subject to various limitations even after the fall of the fascist government in 1943. Laws regulating the Press were present since 1848 (Statuto Albertino), but they were influenced by the ideas of enlightened absolutism. In 1912, Giolitti introduced new liberal laws regarding the Press, as well as universal suffrage, but these laws were repealed by Mussolini during his government. The legal code introduced by Mussolini was modified in 1945, 1951, 1982 and 1999 and is still present nowadays, although they are only minor in entity and they are often completely ignored by the authorities.<br><br>In the Italian Constitution, freedom of press is cited in Art. 21, which states that “Press cannot be subject to authorization or censorship” and defines the cases in which Press can indeed be seized as “absolutely urgent”. However, the same article forbids shows, printed publications, and all other displays which are contrary to morality, and this is a huge problem. Morality is in constant change, and is personal to everyone, making this comma very dubious and hard to use.<br><br>The article, in the end, only guarantees the freedom of press, but limits one’s freedom of expression. Furthermore, it has been the cause of various controversies in the past regarding TV, which has always seen the dominance of the TV emittent Rai.",
         rank: "41st",
-        color: "rgb(255, 255, 100)",
+        color: "rgb(249, 231, 46)",
 
     },
     {
@@ -45,7 +43,7 @@ let countries = [
         linksTo: "#USA",
         desc: "In the USA, freedom of the press is cited in the First Amendment of the Constitution, which states:<br><br><i>Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.</i><br><br>Even if freedom of press is included in the broader freedom of speech, in the Constitution they are separate from each other as to underline the importance of the Press in the USA. However, this freedom is subject to some restrictions, such as the defamation law, which attempts to protect public figures from false allegations but its fallacies can be used against newspapers or publishers by criminals to avoid having information about their crimes circulating. Also, other constraints are caused by the government’s or the public’s hostility and in some cases hatred of journalists.",
         rank: "44th",
-        color: "rgb(255, 255, 100)",
+        color: "rgb(249, 231, 46)",
     },
     {
         alt: "USA",
@@ -54,7 +52,7 @@ let countries = [
         linksTo: "#USA",
         desc: "In the USA, freedom of the press is cited in the First Amendment of the Constitution, which states:<br><br><i>Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.</i><br><br>Even if freedom of press is included in the broader freedom of speech, in the Constitution they are separate from each other as to underline the importance of the Press in the USA. However, this freedom is subject to some restrictions, such as the defamation law, which attempts to protect public figures from false allegations but its fallacies can be used against newspapers or publishers by criminals to avoid having information about their crimes circulating. Also, other constraints are caused by the government’s or the public’s hostility and in some cases hatred of journalists.",
         rank: "44th",
-        color: "rgb(255, 255, 100)",
+        color: "rgb(249, 231, 46)",
     },
     {
         alt: "UK",
@@ -63,7 +61,7 @@ let countries = [
         linksTo: "#UK",
         desc: "Even though the United Kingdom is known to have a long tradition of free and inquisitive press, it has no article to guarantee freedom of press in its Constitution. Up until 1695 a licence was needed for a newspaper to publish articles: this was used by the government to control the media.<br><br>With the lapse of the Licensing Act, greatly influenced by John Locke, the government no longer played a part in censoring publications, and so the right to freedom of press was established in Britain. Despite the UK government’s commitment to defending media freedom, it still has secretly restricted several media outlets, blacklisting them or adopting other measures. Northern Ireland remains a critical zone: because of the conflicts in the territory, many journalists have been arrested or even killed, and their journalistic materials have been seized. Particularly, Lyra McKee was murdered in 2019 while covering paramilitary activity and organised crime in Northern Ireland.",
         rank: "33rd",
-        color: "rgb(255, 255, 100)",
+        color: "rgb(249, 231, 46)",
     },
     {
         alt: "Russia",
@@ -72,7 +70,7 @@ let countries = [
         linksTo: "#Russia",
         desc: "In Russia, freedom of press and speech is guaranteed by the Constitution, but there are many factors, like the selective application of laws, which force the press to self-censorship and avoid certain controversial issues, finally resulting in infringements of the right to free press.<br><br>President Vladimir Putin has done everything in his power to bend the major media outlets to his will, the first one being TV. In 2003, after having censored and taken control over several channels and journals, the government implemented a ban on media outlets campaigning for and against candidates, blatantly violating the freedom of opinion, going as far as killing journalist Anna Politkovskaya in 2006.<br><br>From that point onwards, threats, violent assaults and murders of journalists, together with further restrictions on press and media outlets, began rising at an alarming pace (150 murders are estimated to have happened in the decade 2002-2012). In 2013 an executive order liquidated RIA Novosti, one of the few remaining independent media outlets, and in 2014 the first online news publication service, Grani.ru, was blocked.",
         rank: "150th",
-        color: "rgb(255, 75, 75)",
+        color: "rgb(227, 32, 56)",
     },
     {
         alt: "China",
@@ -81,7 +79,7 @@ let countries = [
         linksTo: "#China",
         desc: "China has one of the world’s most restrictive media environments, and its government heavily relies on censorship to control information in the news, and even on social media. One of the ways they do that is to use every means possible (lawsuits and arrests among the others) to force journalists to censor themselves in order not to get involved with the government.<br><br>Although freedom of speech and press is guaranteed in the Constitution, authorities can block news by stating that they are exposing state secrets (and the definition of those is very vague). Furthermore, in what some scholars call “schizophrenia” of the authorities, the government forbids access to websites like Facebook and Instagram, deemed as dangerous, using the so-called Great Firewall, that blocks each attempt to connect to foreign websites and forces people to use a government substitute of the Internet. The main reason to apply this kind of censorship is to maintain political stability and prevent the government from falling.<br><br>Censorship and limitations are also applied to foreign media outlets, which have to surpass administratives obstacles and obtain several permissions, to prevent journalists from reporting news and information that could potentially damage China’s reputation.",
         rank: "177th",
-        color: "rgb(20, 20, 20)",
+        color: "rgb(35, 31, 32)",
     },
     {
         alt: "Japan",
@@ -90,18 +88,17 @@ let countries = [
         linksTo: "#Japan",
         desc: "Even though the third biggest economic power respects the principles of media freedom and pluralism, many journalists have found difficulties due to the climate of mistrust found in the public towards reporters, which started with the rise to power of the rightwing nationalists since the 2012 general election. Nationalist groups active on social networks harass journalists who cover subjects regarded as “anti-patriotic”, such as the Fukushima nuclear disaster or the presence of US troops in Okinawa, or that criticizes the government. Furthermore, the system of “reporters’ clubs” (kisha clubs) discriminates against foreign and freelancers reporters, and in the meanwhile journalists or bloggers face up to ten years in prison if convicted of publishing information obtained “illegally”, as regulated in a law protecting “Specially-Designated Secrets”, about which the government continues to refuse any debate.",
         rank: "67th",
-        color: "rgb(240, 190, 90)",
+        color: "rgb(247, 153, 38)",
     }
 ];
+
+
 
 let main = document.getElementById("main");
 
 let M = document.getElementById("wMap");
 let ratio = document.getElementById("mainMap").getBoundingClientRect().width / 6450;
 let divs;
-
-
-
 
 
 function show(index) {
@@ -123,6 +120,8 @@ function show(index) {
 
 }
 
+
+
 for (let c of countries) {
 
     // Modal
@@ -134,7 +133,7 @@ for (let c of countries) {
     let content = document.createElement("div");
     content.classList += "content";
     content.style.backgroundColor = c.color;
-    if (c.color == "rgb(20, 20, 20)") {
+    if (c.color == "rgb(35, 31, 32)" || c.color == "rgb(227, 32, 56)") {
         content.style.color = "white";
     } else {
         content.style.color = "black";
@@ -175,7 +174,6 @@ for (let c of countries) {
     A.classList += "nationArea";
     A.addEventListener("click", () => {
         show(getElIndex(A));
-        console.log("")
     });
 
     let scaledCs = "";
@@ -183,6 +181,7 @@ for (let c of countries) {
         let newC = Math.round(coord * ratio);
         scaledCs += newC.toString() + ",";
     }
+    console.log(scaledCs)
     A.coords = scaledCs;
 
     M.appendChild(A);
